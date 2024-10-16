1ec36560b437db8ba5f11824b8d04af5f1674097
Will Poulson <willpoulson44@gmail.com>
2024-10-16 18:08:45 +0100

    Initial commit

---
+++ src/main/java/uk/co/willpoulson/willsfarmingtweaks/WillsFarmingTweaksDataGenerator.java
@@ -0,0 +1,11 @@
+package uk.co.willpoulson.willsfarmingtweaks;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class WillsFarmingTweaksDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
