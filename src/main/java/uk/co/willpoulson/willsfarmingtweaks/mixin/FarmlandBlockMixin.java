1dd84d711ab792bd3727d0cccfb974ed7a21aacb
Will Poulson <willpoulson44@gmail.com>
2025-03-25 18:42:36 +0000

    task(project): update to 1.21.5 and bump project version to 1.0.4

--- src/main/java/uk/co/willpoulson/willsfarmingtweaks/mixin/FarmlandBlockMixin.java
+++ src/main/java/uk/co/willpoulson/willsfarmingtweaks/mixin/FarmlandBlockMixin.java
@@ -13,7 +13,7 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 @Mixin(FarmlandBlock.class)
 public class FarmlandBlockMixin {
 	@Inject(method = "onLandedUpon", at = @At("HEAD"), cancellable = true)
-	private void preventFarmlandTrample(World world, BlockState state, BlockPos pos, Entity entity, float fallDistance, CallbackInfo ci) {
+	private void preventFarmlandTrample(World world, BlockState state, BlockPos pos, Entity entity, double fallDistance, CallbackInfo ci) {
 		ci.cancel();
 	}
 }
